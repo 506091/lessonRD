fa44aff135a3fab8afdb39b2302625d33c422361
Volodymyr Hlukhonets <506091@gmail.com>
2023-01-16 18:16:30 +0100

    homework2 new branch HWVH

---
+++ src/com/ua/robot/lesson3/Main.java
@@ -0,0 +1,14 @@
+package com.ua.robot.lesson3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int year = 2023;
+        double percent = 7.8;
+        short s = 5;
+        byte b = 127;
+        b++;
+        System.out.println(b);
+    }
+
+}
