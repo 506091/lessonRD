c917536ac5ff551ba683165e400ddc8fdfbf6c3b
Volodymyr Hlukhonets <506091@gmail.com>
2023-01-22 15:16:31 +0100

    homework4 newbranch 4 HomeWork 4

---
+++ src/com/ua/robot/HomeWork4string/HomeWorkStringVH4.java
@@ -0,0 +1,26 @@
+package com.ua.robot.HomeWork4string;
+
+import java.util.Scanner;
+
+public class HomeWorkStringVH4 {
+
+    public static void main(String[] args) {
+
+        Scanner EnterString = new Scanner(System.in);
+        System.out.println("Введіть текст для обробки: ");
+        String MyHomeWorkString = EnterString.next();
+        System.out.println("Ваш текст: "+MyHomeWorkString);
+
+        System.out.println("UpperCase: "+MyHomeWorkString.toUpperCase());
+        System.out.println("LowerCase: "+MyHomeWorkString.toLowerCase());
+
+        System.out.println("Перша літера/знак: "+MyHomeWorkString.charAt(0));
+
+        int lenthMyString = MyHomeWorkString.length();
+        int lastIndexMyString = lenthMyString - 1;
+
+        System.out.println("Остання літера/знак: "+MyHomeWorkString.charAt(lastIndexMyString));
+
+    }
+
+}
