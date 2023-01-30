3fbb39d3a75fcef7da4376887836b2e1a7626096
Volodymyr Hlukhonets <506091@gmail.com>
2023-01-30 11:18:24 +0100

    My HW 6 110

--- src/com/ua/robot/HomeWork6/HW6.java
+++ src/com/ua/robot/HomeWork6/HW6.java
@@ -2,10 +2,10 @@ package com.ua.robot.HomeWork6;
 
 public class HW6 {
     public static void main(String[] args) {
-        int OneTen = 1;
+        int i = 1;
         System.out.print("Від 1 до 10: ");
-        for (; OneTen <= 10; OneTen++){
-            System.out.print(OneTen+" ");
+        for (; i <= 10; i++){
+            System.out.print(i+" ");
         }
     }
 }
