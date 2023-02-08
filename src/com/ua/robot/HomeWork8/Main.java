0eecae2bbe480621133e828fb3236b73c4710e9e
Volodymyr Hlukhonets <506091@gmail.com>
2023-02-08 20:33:30 +0100

    HomeWork8end)

--- src/com/ua/robot/HomeWork8/Main.java
+++ src/com/ua/robot/HomeWork8/Main.java
@@ -12,14 +12,22 @@ public class Main {
 //        for (int i = 0; i < arrHW.length; i++){
 //            System.out.print(arrHW[i]+" ");
 //            }
-//        System.out.println();
+        System.out.println();
 
         int maxArr = getMaxArr(arrHW);
         System.out.println("Максимальне значення в масиві: "+maxArr);
 
         int minArr = getMinArr(arrHW);
         System.out.println("Мінімальне значення в масиві: "+minArr);
+
+        int sumArr = getSum(arrHW);
+        System.out.println("Сума усіх елементів масиву: "+sumArr);
+
+        double averageArr = getAverageArr(arrHW, sumArr);
+        System.out.println("Середнє значення з усіх елементів масиву: "+ averageArr);
+
     }
+
 
     static  int [] arrTenElements(int yourLength){
         int [] arrHW = new int[yourLength];
@@ -45,6 +53,17 @@ public class Main {
                 minValue = inputArray[i];
             }
         } return minValue;
+    }
+
+    static int getSum(int[] inputArray){
+        int sumArr = 0;
+        for (int i = 0; i < inputArray.length; i++){
+            sumArr += inputArray[i];
+        }return sumArr;
+    }
+
+    static double getAverageArr(int[] inputArray, int sumArr){
+        return (double) sumArr / inputArray.length;
     }
 
 }
