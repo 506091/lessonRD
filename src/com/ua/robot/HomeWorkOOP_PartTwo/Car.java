20de3011412c0c25a5996002fffa85ecc6791aef
Volodymyr Hlukhonets <506091@gmail.com>
2023-02-19 17:06:55 +0100

    ООП. Частина 2

---
+++ src/com/ua/robot/HomeWorkOOP_PartTwo/Car.java
@@ -0,0 +1,21 @@
+package com.ua.robot.HomeWorkOOP_PartTwo;
+
+public class Car {
+    protected String carManufacturer;
+
+
+    public String getCarManufacturer() {
+        return carManufacturer;
+    }
+
+    public void setCarManufacturer(String carManufacturer) {
+        this.carManufacturer = carManufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carManufacturer='" + carManufacturer + '\'' +
+                '}';
+    }
+}
---
+++ src/com/ua/robot/HomeWorkOOP_PartTwo/Trucks.java
@@ -0,0 +1,36 @@
+package com.ua.robot.HomeWorkOOP_PartTwo;
+
+public class Trucks extends Car{
+    private String trucksModel;
+
+    private int yearModelTrucks;
+
+    public String getTrucksModel() {
+        return trucksModel;
+    }
+
+    public void setTrucksModel(String trucksModel) {
+        this.trucksModel = trucksModel;
+    }
+
+    public int getYearModelTrucks() {
+        return yearModelTrucks;
+    }
+
+    public void setYearModelTrucks(int yearModelTrucks) {
+        this.yearModelTrucks = yearModelTrucks;
+    }
+
+    @Override
+    public String toString() {
+        return "Trucks{" +
+                "carManufacturer='" + getCarManufacturer() + '\'' +
+                "trucksModel='" + trucksModel + '\'' +
+                ", yearModelTrucks=" + yearModelTrucks +
+                '}';
+    }
+
+    public String getType(){
+        return "у нас це Trucks " + getCarManufacturer();
+    }
+}
