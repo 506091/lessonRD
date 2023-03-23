cda0d1f487d1c0d7befc922948a4309fd9c33595
Volodymyr Hlukhonets <506091@gmail.com>
2023-03-23 19:53:29 +0100

    ООП. ListPart1

---
+++ src/com/ua/robot/List1/Student.java
@@ -0,0 +1,35 @@
+package com.ua.robot.List1;
+
+public class Student {
+
+    private String name;
+    private String lastName;
+    private int age;
+
+    public Student(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
